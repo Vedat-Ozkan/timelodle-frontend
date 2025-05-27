a37ad25b23f23808fbb2e3b24e170688a1abf2c9
vkozkan <vedatkemalozkan@gmail.com>
2025-05-27 15:42:14 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import './App.css'
+import MyTimeline from './components/timeline';
 
 function App() {
+  axios
+    .get(`${process.env.REACT_APP_API_URL}/api/five/`)
+    .then((res) => console.log(res));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyTimeline />
+    </>
   );
 }
 
